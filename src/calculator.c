b6099f37d5ed8937e15c54e64bc028fd9caf937e
Denis Putnov <grnbows.connect@mail.ru>
2020-03-25 03:21:06 +0400

    Added file support

--- src/calculator.c
+++ src/calculator.c
@@ -19,119 +19,243 @@ int main(void)
     // variable description
     float a,b,res,*x,*y;
     int k,factRes,size,i;
-    char c;
+    char c,l,v;
 
-    do
-    {
-        printf("Enter operation (+ - / * ! ^) for numbers and\n");  // prints operations list on the screen
-        printf("enter operation (p, s) [plus, subtraction] for vectors:");
-        scanf(" %c", &c);    // scan operation
-        switch(c)  // 'plus' operation cycle
+
+        printf("What are you want to use? File or console? [f/c]");
+        scanf(" %c", &l);
+        switch(l)  //  choose with what are you want to work, console of file
             {
-        case '+':
+        case 'f':
             {
-                printf("Enter the first number:");
-                scanf("%f", &a);
-                printf("Enter the second number:");
-                scanf("%f", &b);
-                printf("Result is %.2f (operation %c)\n",a+b,c);
+                FILE *input, *output;  // define files
+                input = fopen("input.txt","r");  // open all files that we need
+                output = fopen("output.txt", "w");
+                fscanf(input, " %c", &c); // scan operation
+                fscanf(input, " %c", &v); // scan vector or number
+                switch(v)  // choose vector or number
+                {
+                case 's': // if number
+                {
+                    switch(c)
+                    {
+                    case '+':
+                        {
+                            fscanf(input, " %f", &a);
+                            fscanf(input, " %f", &b);
+                            fprintf(output, "%f + %f = %f", a, b, a+b);
+                            break;
+                        }
+                    case '-':
+                        {
+                            fscanf(input, " %f", &a);
+                            fscanf(input, " %f", &b);
+                            fprintf(output, "%f - %f = %f", a, b, a-b);
+                            break;
+                        }
+                    case '/':
+                        {
+                            fscanf(input, " %f", &a);
+                            fscanf(input, " %f", &b);
+                            if (b!=0) fprintf(output, "%f / %f = %f", a, b, a/b);
+                            break;
+                        }
+                    case '*':
+                        {
+                            fscanf(input, " %f", &a);
+                            fscanf(input, " %f", &b);
+                            fprintf(output, "%f * %f = %f", a, b, a*b);
+                            break;
+                        }
+                    case '^':
+                        {
+                            fscanf(input, " %f", &a);
+                            fscanf(input, " %i", &k);
+                            res = 1;
+                                for (int i=0; i<k;i++) res = res*a; // just * number on itself for k times
+                            fprintf(output, "%f ^ %i = %f", a, k, res);
+                            break;
+                        }
+                    case '!':
+                        {
+                            factRes = 1;
+                            fscanf(input," %i",&k);
+                            if (k >= 0) for (int i=k; i!=1;i--) factRes = factRes*i;    // calculate factorial from back
+                            else fprintf(output, "This number is below zero. Please, try again.");
+                            fprintf(output, "!%i = %i", k, factRes);
+                            break;
+                        }
+                    }
+                break;
+                }
+
+                case 'v':  // if vector
+                {
+                    x=malloc(size*sizeof(int));
+                    y=malloc(size*sizeof(int));
+                    switch(c)
+                    {
+                    case '+':
+                        {
+                            fscanf(input, " %i", &size);
+                            for(int i=0;i<size;i++)
+                                fscanf(input, " %f",&x[i]);
+                            for(int i=0;i<size;i++)
+                                fscanf(input, " %f",&y[i]);
+                            fprintf(output, "( ");
+                            for(int i=0;i<size;i++)
+                                fprintf(output, "%f ", x[i]);
+                            fprintf(output, ") + ( ");
+                            for(int i=0;i<size;i++)
+                                fprintf(output, "%f ", y[i]);
+                            fprintf(output, ") = ( ");
+                            for(int i=0;i<size;i++)
+                                fprintf(output, "%.2f ",x[i]+y[i]);
+                            fprintf(output, ")");
+                            break;
+                        }
+                    case '-':
+                        {
+                            fscanf(input, " %i", &size);
+                            for(int i=0;i<size;i++)
+                                fscanf(input, " %f",&x[i]);
+                            for(int i=0;i<size;i++)
+                                fscanf(input, " %f",&y[i]);
+                            fprintf(output, "( ");
+                            for(int i=0;i<size;i++)
+                                fprintf(output, "%f ", x[i]);
+                            fprintf(output, ") + ( ");
+                            for(int i=0;i<size;i++)
+                                fprintf(output, "%f ", y[i]);
+                            fprintf(output, ") = ( ");
+                            for(int i=0;i<size;i++)
+                                fprintf(output, "%.2f ",x[i]-y[i]);
+                            fprintf(output, ")");
+                            break;
+                        }
+                    }
+                    free(x);  //  make memory free
+                    free(y);
+                break;
+                }
+                }
+                fclose(input); //  close files
+                fclose(output);
                 break;
             }
-        case '-':  // 'minus' operation cycle
+        case 'c':
             {
-                printf("Enter the first number:");
-                scanf("%f", &a);
-                printf("Enter the second number:");
-                scanf("%f", &b);
-                printf("Result is %.2f (operation %c)\n", a-b,c);
-                break;
-            }
-        case '/':  // 'divide' operation cycle
-            {
-                printf("Enter the first number:");
-                scanf("%f", &a);
-                printf("Enter the second number:");
-                scanf("%f", &b);
-                if (b != 0) printf("Result is %.4f (operation %c)\n", a/b,c); // you can divide something only if second number != 0. 'Idiot check'
-                else printf("You can't divide by zero\n");
-                break;
-            }
-        case '*':  // 'multiplication' operation cycle
-            {
-                printf("Enter the first number:");
-                scanf("%f", &a);
-                printf("Enter the second number:");
-                scanf("%f", &b);
-                printf("Result is %.2f (operation %c)\n", a*b,c);
-                break;
-            }
-        case '^':  // exponent cycle
-            {
-                printf("Enter the number:");
-                scanf("%f", &a);
-                printf("Enter the exponent (!only integer!):");
-                scanf("%i", &k);
-                res = 1;
-                    for (int i=0; i<k;i++) res = res*a; // just * number on itself for k times
-                printf("Result is %.2f\n", res);
-                break;
-            }
-        case '!':  // factorial calculating
-            {
-                factRes = 1;
-                printf("Enter the number (!only integer!):");
-                scanf("%i",&k);
-                if (k >= 0) for (int i=k; i!=1;i--) factRes = factRes*i;    // calculate factorial from back
-                else printf("This number is below zero. Please, try again.");
-                printf("Result is %i\n", factRes);
-                break;
-            }
-        case 'p':
-            {
-                i=0;
-                x=malloc(size*sizeof(int));
-                y=malloc(size*sizeof(int));
-                printf("Enter vector's size: ");
-                scanf(" %i",&size);
-                printf("Enter first vector: ");
-                for(int i=0;i<size;i++)
-                    scanf(" %f",&x[i]);
-                printf("Enter second vector: ");
-                for(int i=0;i<size;i++)
-                    scanf(" %f",&y[i]);
-                printf("Result vectors summ: ");
-                for(int i=0;i<size;i++)
-                    printf("%.2f ",x[i]+y[i]);
-                printf("\n");
-                free(x);
-                free(y);
-                free(res);
-                break;
-            }
-         case 's':
-            {
-                i=0;
-                x=malloc(size*sizeof(int));
-                y=malloc(size*sizeof(int));
-                printf("Enter vector's size: ");
-                scanf(" %i",&size);
-                printf("Enter first vector: ");
-                for(int i=0;i<size;i++)
-                    scanf(" %f",&x[i]);
-                printf("Enter second vector: ");
-                for(int i=0;i<size;i++)
-                    scanf(" %f",&y[i]);
-                printf("Result vectors subtraction : ");
-                for(int i=0;i<size;i++)
-                    printf("%.2f ",x[i]-y[i]);
-                printf("\n");
-                free(x);
-                free(y);
-                free(res);
-                break;
+            do
+                {
+                printf("Enter operation (+ - / * ! ^) for numbers and\n");  // prints operations list on the screen
+                printf("enter operation (p, s) [plus, subtraction] for vectors:");
+                scanf(" %c", &c);    // scan operation
+                switch(c)
+                    {
+                case '+':  // 'plus' operation cycle
+                    {
+                        printf("Enter the first number:");
+                        scanf("%f", &a);
+                        printf("Enter the second number:");
+                        scanf("%f", &b);
+                        printf("Result is %.2f (operation %c)\n",a+b,c);
+                        break;
+                    }
+                case '-':  // 'minus' operation cycle
+                    {
+                        printf("Enter the first number:");
+                        scanf("%f", &a);
+                        printf("Enter the second number:");
+                        scanf("%f", &b);
+                        printf("Result is %.2f (operation %c)\n", a-b,c);
+                        break;
+                    }
+                case '/':  // 'divide' operation cycle
+                    {
+                        printf("Enter the first number:");
+                        scanf("%f", &a);
+                        printf("Enter the second number:");
+                        scanf("%f", &b);
+                        if (b != 0) printf("Result is %.4f (operation %c)\n", a/b,c); // you can divide something only if second number != 0. 'Idiot check'
+                        else printf("You can't divide by zero\n");
+                        break;
+                    }
+                case '*':  // 'multiplication' operation cycle
+                    {
+                        printf("Enter the first number:");
+                        scanf("%f", &a);
+                        printf("Enter the second number:");
+                        scanf("%f", &b);
+                        printf("Result is %.2f (operation %c)\n", a*b,c);
+                        break;
+                    }
+                case '^':  // exponent cycle
+                    {
+                        printf("Enter the number:");
+                        scanf("%f", &a);
+                        printf("Enter the exponent (!only integer!):");
+                        scanf("%i", &k);
+                        res = 1;
+                            for (int i=0; i<k;i++) res = res*a; // just * number on itself for k times
+                        printf("Result is %.2f\n", res);
+                        break;
+                    }
+                case '!':  // factorial calculating
+                    {
+                        factRes = 1;
+                        printf("Enter the number (!only integer!):");
+                        scanf("%i",&k);
+                        if (k >= 0) for (int i=k; i!=1;i--) factRes = factRes*i;    // calculate factorial from back
+                        else printf("This number is below zero. Please, try again.");
+                        printf("Result is %i\n", factRes);
+                        break;
+                    }
+                case 'p':
+                    {
+                        i=0;
+                        x=malloc(size*sizeof(int));
+                        y=malloc(size*sizeof(int));
+                        printf("Enter vector's size: ");
+                        scanf(" %i",&size);
+                        printf("Enter first vector: ");
+                        for(int i=0;i<size;i++)
+                            scanf(" %f",&x[i]);
+                        printf("Enter second vector: ");
+                        for(int i=0;i<size;i++)
+                            scanf(" %f",&y[i]);
+                        printf("Result vectors summ: ");
+                        for(int i=0;i<size;i++)
+                            printf("%.2f ",x[i]+y[i]);
+                        printf("\n");
+                        free(x);
+                        free(y);
+                        break;
+                    }
+                 case 's':
+                    {
+                        i=0;
+                        x=malloc(size*sizeof(int));
+                        y=malloc(size*sizeof(int));
+                        printf("Enter vector's size: ");
+                        scanf(" %i",&size);
+                        printf("Enter first vector: ");
+                        for(int i=0;i<size;i++)
+                            scanf(" %f",&x[i]);
+                        printf("Enter second vector: ");
+                        for(int i=0;i<size;i++)
+                            scanf(" %f",&y[i]);
+                        printf("Result vectors subtraction : ");
+                        for(int i=0;i<size;i++)
+                            printf("%.2f ",x[i]-y[i]);
+                        printf("\n");
+                        free(x);
+                        free(y);
+                        break;
+                    }
+                    }
+                printf("\nTry again? Type any symbol to continue and 'q' for exit.");
+                scanf(" %c", &c);
+                } while (c != 'q');    // program working status
             }
         }
-        printf("\nTry again? Type any symbol to continue and 'q' for exit.");
-        scanf(" %c", &c);
-    } while (c != 'q');    // program working status
 }
